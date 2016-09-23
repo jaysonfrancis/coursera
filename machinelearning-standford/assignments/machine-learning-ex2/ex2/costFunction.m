be17011471283cb33d010f93f80c131664b5156b
Jayson Francis <jaysonfrancis@gmail.com>
2016-09-23 16:27:46 -0700

    Completed ex2

--- machinelearning-standford/assignments/machine-learning-ex2/ex2/costFunction.m
+++ machinelearning-standford/assignments/machine-learning-ex2/ex2/costFunction.m
@@ -23,7 +23,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X*theta); 
 
+J = (1/m)*(-y' * log(h) - (1-y)' * log(1-h));
+
+grad = (1/m) * X' * (h-y);
 
 
 
--- machinelearning-standford/assignments/machine-learning-ex2/ex2/costFunctionReg.m
+++ machinelearning-standford/assignments/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,15 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
 
+shift = theta(2:size(theta));
 
+theta_reg = [0; shift];
 
+J = (1/m)*(-y' * log(h) - (1-y)' * log(1-h)) + (lambda/(2*m)) * theta_reg' * theta_reg;
 
+grad = (1/m)*(X'*(h-y)+lambda*theta_reg);
 
 % =============================================================
 
